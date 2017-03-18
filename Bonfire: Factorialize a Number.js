ecd65522cda87fc3dfe8d8d21b39ce12ea0ca28c
Celayna <kri2074314@maricopa.edu>
2017-03-17 18:37:38 -0700

    Update Bonfire: Factorialize a Number.js

--- Bonfire: Factorialize a Number.js
+++ Bonfire: Factorialize a Number.js
@@ -6,7 +6,7 @@ If the integer is represented with the letter n, a factorial is the product of a
 Factorials are often represented with the shorthand notation n!
 
 For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-/*
+*/
 
 //My Solution:
 
